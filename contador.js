f4eb4fe619461e0a24cc879173d46091abd45122
Paulo Alba <pr2013com@hotmail.com>
2022-05-26 01:47:08 -0300

    javascript adicionado

---
+++ contador.js
@@ -0,0 +1,164 @@
+// componentes teclado
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let btn3 = document.querySelector(".btn3")
+let btn4 = document.querySelector(".btn4")
+let btn5 = document.querySelector(".btn5")
+let btn6 = document.querySelector(".btn6")
+let btn7 = document.querySelector(".btn7")
+let btn8 = document.querySelector(".btn8")
+let btn9 = document.querySelector(".btn9")
+let btnZero = document.querySelector(".zero")
+let btnDot = document.querySelector(".dot")
+
+
+let btnMais = document.querySelector(".contMais")
+let btnMenos = document.querySelector(".contMenos")
+let btnBackspace = document.querySelector(".btn-backspace")
+
+let qtd = document.querySelector(".qtd").textContent = 1
+
+let multplo = 1
+
+btnMais.addEventListener("click", function ()  {
+    multplo += 1
+    document.querySelector(".qtd").textContent = multplo
+    
+})
+btnMenos.addEventListener("click", function () {
+    if(multplo != 1){
+        multplo += -1
+        document.querySelector(".qtd").textContent = multplo
+    }
+ })
+    
+
+let btnAdicionar = document.querySelector(".btn-adicionar")
+let btnZerar = document.querySelector(".zerar")
+
+let total = document.querySelector(".total")
+total = 0
+
+btnAdicionar.addEventListener("click", somar)
+btnZerar.addEventListener("click", zerar)
+
+function somar(){
+    let containerInput = document.querySelector(".containerInput").textContent
+    if(containerInput != ""){
+        
+        num1 = Number(containerInput)
+        total += num1 * multplo
+        document.querySelector(".total").textContent = total
+        document.querySelector(".containerInput").textContent = ""
+        let li = document.createElement("li")
+        li.textContent = num1 + " (" + multplo + ")"
+        let ul = document.querySelector("ul")
+        ul.appendChild(li)
+    } 
+}
+
+function zerar(){
+    location.reload()
+    let li = document.querySelectorAll("li")
+    let list = Array.from(li)
+    document.querySelector(".total").textContent = 0
+    document.querySelector(".qtd").textContent = 1
+    for(let i = 0; i < list.length; i++){
+    list[i].classList.add("remover")
+}
+}
+
+btnBackspace.addEventListener("click", backspace)
+
+function backspace(){
+
+    words = document.querySelector(".containerInput").textContent = ""
+
+
+
+}
+
+// botões
+
+
+
+btn1.addEventListener("click", digito1)
+
+
+function digito1 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 1
+}
+
+
+btn2.addEventListener("click", digito2)
+
+
+function digito2 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 2
+}
+
+
+btn3.addEventListener("click", digito3)
+
+function digito3 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 3
+}
+
+btn4.addEventListener("click", digito4)
+
+function digito4 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 4
+}
+
+btn5.addEventListener("click", digito5)
+
+function digito5 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 5
+}
+
+btn6.addEventListener("click", digito6)
+
+function digito6 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 6
+}
+
+btn7.addEventListener("click", digito7)
+
+function digito7 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 7
+}
+btn8.addEventListener("click", digito8)
+
+function digito8 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 8
+}
+btn9.addEventListener("click", digito9)
+
+function digito9 (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 9
+}
+btnZero.addEventListener("click", digitoZero)
+
+function digitoZero (){
+    let containerInput = document.querySelector(".containerInput")
+    containerInput.textContent += 0
+}
+
+btnDot.addEventListener("click", digitoDot)
+
+function digitoDot (){
+    let containerInput = document.querySelector(".containerInput")
+    if(containerInput.textContent != ""){
+       return containerInput.textContent += "."
+    } 
+}
+
