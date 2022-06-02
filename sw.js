bd9de6f7f8eae3bbde349f43050b73751b3db479
Paulo Alba <pr2013com@hotmail.com>
2022-06-01 22:31:58 -0300

    css para PWA

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ self.addEventListener('install', function(event){
                 './',
                 './index.html',
                 './manifest.webmanifest',
+                './style.css',
                 './index.js'
             ])
         })
